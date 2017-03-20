eb2b912a84459cbf4d9aaff2a9d5043dee3ec67a
Freyr <sky_rider_93@mail.ru>
2017-03-20 14:29:38 +0300

    editor option implemented

--- pipe.sh
+++ pipe.sh
@@ -1,41 +1,45 @@
 #!/bin/bash
 
-pipe=./testpipe
-process=$@
+PIPE=/tmp/pipe
+PROC=$@
+TMPFILE="/tmp/tmp.pipe"
+: ${EDITOR:="vi"}
 
 if [ $# -eq 0 ]; then
     echo "No arguments supplied"
     exit 0
 fi
 
-if [[ ! -p $pipe ]]; then
-    mkfifo $pipe
+if [[ ! -p $PIPE ]]; then
+    mkfifo $PIPE
 fi
 
-$process < $pipe &
-pid=$!
+$PROC < $PIPE &
+PID=$!
 
-echo "Executed process with PID $pid"
+echo "Executed process with PID $PID"
 
 # if the script is killed, kill the process
-trap "kill $pid 2> /dev/null" EXIT
+trap "kill $PID 2> /dev/null" EXIT
 
-while kill -0 $pid 2> /dev/null
+while kill -0 $PID 2> /dev/null
 do
-    if read -rsn1; then
-        touch ./tmp
-        vim -c 'startinsert' ./tmp
-        txt=$(<./tmp)
-        if [[ "$txt" == 'quit' ]]; then
-            break
-        fi
-        # echo $txt > $pipe
-        if [ -s "./tmp" ]; then
-            echo $txt > $pipe
-        fi
-        rm ./tmp
+    if read -rs -n 1 -p "Options: [e]dit, [q]uit" OPTION; then
+	echo ""
+   	echo "" > $TMPFILE
+	case "$OPTION" in
+		q) 
+			break
+			;;
+		e)
+        		$EDITOR $TMPFILE
+        		if [ -s "$TMPFILE" ]; then
+          			cat $TMPFILE > $PIPE
+        		fi
+			;;
+	esac
     fi
-done 3>$pipe
+done 3>$PIPE
 
 echo "Pipe script exiting"
 
